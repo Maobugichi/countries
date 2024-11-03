8780f79ca641fe093ff287ae80c85dbff2aa77aa
Maobugichi <immansamuel846@gmail.com>
2024-11-03 12:47:05 -0800

    final3

--- src/routes/Filter.jsx
+++ src/routes/Filter.jsx
@@ -36,7 +36,7 @@ export default function Filter({data,setIsData}) {
    }
   return(
     <div className="relative transition-all duration-300 lg:h-[60px] h-full w-[95%] mx-auto flex lg:flex-row flex-col gap-4 justify-between lg:mt-[7%] mt-[30%] ">
-      <svg className="absolute top-4 left-4 lg:top-6 lg:left-8" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg>
+      <svg className="absolute top-7 left-4 lg:top-6 lg:left-8" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg>
       <Input
        placeholder="Search for a country..."
        data={newData}

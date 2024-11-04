3d8d3ed3d51d2032a89eceaa232f6eebbe2988a3
Maobugichi <immansamuel846@gmail.com>
2024-11-04 01:41:57 -0800

    finals3

--- src/routes/CountriesList.jsx
+++ src/routes/CountriesList.jsx
@@ -1,3 +1,3 @@
 export default function CountriesList({children}) {
-    return <ul role="list" className="flex flex-col lg:flex-row gap-8 flex-wrap lg:w-[95%] w-full mx-auto">{children}</ul>
+    return <ul className="flex flex-col lg:flex-row gap-8 flex-wrap lg:w-[95%] w-full mx-auto">{children}</ul>
 }
